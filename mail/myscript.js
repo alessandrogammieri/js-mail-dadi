3950702c628c0993b7187d23ce8935ced6a42f36
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-02 17:01:50 +0200

    email control

--- mail/myscript.js
+++ mail/myscript.js
@@ -4,6 +4,53 @@ chi può accedere e stampa un messaggio
 appropriato; */
 
 // Dichiarazione delle variabili
-console.log("ciao mondo");
+// var trovato, yourmail, email;
+//
+// // Invocazione delle variabili
+//   // Variabile standart "trovato"
+// trovato = false
+//
+//   // Chiediamo all'utente il suo indirizzo Email
+// yourmail = prompt("Inserisci il tuo indirizzo email");
+//
+//   // Lista delle nostre mail
+// email = ["alexgam@live.it", "alefra@gmail.com", "alessandrogammieri@gmail.com"];
+//
+//   // Verifichiamo se l'indirizzo è presente nella nostra lista
+// for (var i = 0; i < email.length; i++) {
+//   if (email[i] == yourmail) {
+//     trovato = true;
+//   }
+// }
+//
+//   // Comunichiamo all'utente se sarà o meno invitato alla festa
+// if (trovato == true) {
+//   document.getElementById('party').innerHTML = "Sei invitato";
+// } else {
+//   document.getElementById('party').innerHTML = "Sarà per la prossima";
+// }
+
+// SECONDA SOLUZIONE -----------------------------------------------------------
+
+// Dichiarazione delle variabili
+var sms, yourmail, email;
 
 // Invocazione delle variabili
+  // Variabile standart "sms"
+sms = "Sarà per la prossima";
+
+  // Chiediamo all'utente il suo indirizzo Email
+yourmail = prompt("Inserisci il tuo indirizzo email");
+
+  // Lista delle nostre mail
+email = ["alexgam@live.it", "alefra@gmail.com", "alessandrogammieri@gmail.com"];
+
+  // Verifichiamo se l'indirizzo è presente nella nostra lista
+for (var i = 0; i < email.length; i++) {
+  if (email[i] == yourmail) {
+    sms = "Sei invitato";
+  }
+}
+
+  // Comunichiamo all'utente se sarà o meno invitato alla festa
+document.getElementById('party').innerHTML = sms;
