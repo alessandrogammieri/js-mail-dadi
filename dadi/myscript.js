9d625ab9e1c49c90edcc1043491c6e9c567eec9d
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-02 18:59:56 +0200

    dice game

--- dadi/myscript.js
+++ dadi/myscript.js
@@ -6,6 +6,25 @@ chi ha vinto, se ha vinto qualcuno. */
 
 
 // Dichiarazione delle variabili
-console.log("ciao a tutti!");
+var x, y, risultato;
 
 // Invocazione delle variabili
+  // Lancio
+function myFunction() {
+  x = Math.floor((Math.random() * 6) + 1);
+  document.getElementById("first").innerHTML = x;
+  y = Math.floor((Math.random() * 6) + 1);
+  document.getElementById("second").innerHTML = y;
+
+  // Si verifica quale numero ha vinto o se c'è stato un pareggio
+  if (x > y) {
+    risultato = "Il Primo concorrente ha vinto";
+  } else if (x == y) {
+      risultato = "Pareggio";
+    } else {
+      risultato = "Il Secondo concorrente ha vinto";
+    }
+
+  // Si comunica chi ha vinto o se c'è stato un pareggio
+  document.getElementById('vincitore').innerHTML = "Risultato: " + risultato;
+}
