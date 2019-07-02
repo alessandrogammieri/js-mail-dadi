f7b6ec9289f93d13acf56c1ec33ada57881783be
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-07-02 14:42:15 +0200

    initialized html + js

---
+++ dadi/myscript.js
@@ -0,0 +1,11 @@
+/* Gioco dei dadi, chi fa di più vince;
+riguarda proprio lo scenario base,
+quindi ci saranno 2 lanci di dadi e
+poi noi dovremmo stampare a schermo
+chi ha vinto, se ha vinto qualcuno. */
+
+
+// Dichiarazione delle variabili
+console.log("ciao a tutti!");
+
+// Invocazione delle variabili
---
+++ mail/myscript.js
@@ -0,0 +1,9 @@
+/* Chiedi all’utente la sua email,
+controlla che sia nella lista di
+chi può accedere e stampa un messaggio
+appropriato; */
+
+// Dichiarazione delle variabili
+console.log("ciao mondo");
+
+// Invocazione delle variabili
